261f81a7518dfebfef06522900adbc1713f54fc8
MazenAbdelateef <mazenkahraba502@gmail.com>
2026-05-25 22:42:12 +0300

    Hiding senstive Google credentials

--- SAKANI/Controllers/AuthController.cs
+++ SAKANI/Controllers/AuthController.cs
@@ -1,7 +1,11 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Sakani.BLL.Core.DTOs.AuthDTOs;
+using Sakani.BLL.Core.DTOs.AuthDTOs.ExternalAuthDTOs;
 using Sakani.BLL.Core.Interfaces.Auth;
+using Sakani.Domain.Entities;
+using System.Security.Claims;
 
 namespace SAKANI.Controllers
 {
@@ -11,11 +15,13 @@ namespace SAKANI.Controllers
     {
         private readonly IAuthService  _authService;
         private readonly IRefreshTokenService _refreshTokenService;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public AuthController(IAuthService authService, IRefreshTokenService refreshTokenService)
+        public AuthController(IAuthService authService, IRefreshTokenService refreshTokenService, SignInManager<ApplicationUser> signInManager)
         {
             _authService  = authService;
             _refreshTokenService = refreshTokenService;
+            _signInManager = signInManager;
         }
 
         [HttpPost("register")]
@@ -61,6 +67,49 @@ namespace SAKANI.Controllers
                 });
             }
         }
+
+
+        // External Login 
+        [HttpGet("External-Login")]
+        public async Task<IActionResult> ExternalLogin(string Provider , string role = "Tenant" ) {
+
+            var redirectUrl = Url.Action(nameof(ExternalCallBack),"Auth");
+            var properties = _signInManager.ConfigureExternalAuthenticationProperties(Provider, redirectUrl);
+            properties.Items["role"] = role; // Pass the role as part of the authentication properties
+            return Challenge(properties,Provider);
+        }
+
+
+
+
+        [HttpGet("External-Login-Callback")]
+        public async Task<IActionResult> ExternalCallBack() {
+            var Info = await _signInManager.GetExternalLoginInfoAsync();
+            if (Info == null)
+                return BadRequest(new { message = "Error loading external login information from provider." });
+
+            var ReqRole = Info.AuthenticationProperties.Items.ContainsKey("requestedRole") ? Info.AuthenticationProperties.Items["requestedRole"] : "Tenant";
+
+            var Email = Info.Principal.FindFirstValue(System.Security.Claims.ClaimTypes.Email);
+            var Name = Info.Principal.FindFirstValue(ClaimTypes.Name);
+
+            if (string.IsNullOrEmpty(Email))
+                return BadRequest(new { message = "Email claim not found in external login information." });
+
+            var extDto = new ExternalAuthDto
+            {
+             Email = Email,
+             Name = Name,
+             ProviderKey = Info.ProviderKey,
+             ProviderName = Info.LoginProvider,
+             RequestedRole = ReqRole
+            };
+
+            return Ok(extDto);
+        }
+
+
+
 
         [HttpPost("refresh-token")]
         [AllowAnonymous]
