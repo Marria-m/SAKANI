d4de50a3570a6990095952b8b21bff6f2a8e813c
MazenAbdelateef <mazenkahraba502@gmail.com>
2026-07-14 23:06:21 +0300

    making three mehtod for the wishlist Repo

--- Sakani.DAL/Repositories/WishListRepository.cs
+++ Sakani.DAL/Repositories/WishListRepository.cs
@@ -2,34 +2,39 @@ using Microsoft.EntityFrameworkCore;
 using Sakani.DAL.Data.Context;
 using Sakani.Domain.Entities;
 using Sakani.Domain.Interfaces;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Sakani.DAL.Repositories
 {
-    public class WishListRepository : GenericRepository<WishListApartment>//, IWishListRepository
+    public class WishListRepository : GenericRepository<WishListApartment>, IWishListRepository
     {
         public WishListRepository(AppDbContext context) : base(context) { }
 
-        //public async Task<IReadOnlyList<WishListApartment>> GetByTenantIdAsync(int tenantId)
-        //{
-        //    return await _dbSet
-        //        .Where(w => w.TenantId == tenantId)
-        //        .Include(w => w.WishListApartments)
-        //        .ToListAsync();
-        //}
+        public  async Task<(bool IsSuccess, string Message)> ExistsAsync(int tenantId, int apartmentId)
+        {
+            var target = await _context.WishListApartments.AnyAsync(w => w.TenantId == tenantId && w.ApartmentId == apartmentId);
+            if (!target) 
+                return (false, $"Apartment ID {apartmentId} was not found in Tenant ID {tenantId}'s wishlist.");
 
-        //public async Task<bool> ExistsAsync(int tenantId, int apartmentId)
-        //{
-        //    return await _dbSet
-        //        .AnyAsync(w => w.TenantId == tenantId && w.Apartments
-        //        .Any(a => a.Id == apartmentId));
-        //}
-        public async Task<WishListApartment?> GetEntryAsync(int tenantId, int apartmentId)
+            return (true, $"Apartment ID {apartmentId} exists in Tenant ID {tenantId}'s wishlist.");
+        }
+
+        public async Task<IReadOnlyList<WishListApartment>> GetByTenantIdAsync(int tenantId)
         {
             return await _context.WishListApartments
-                .FirstOrDefaultAsync(w => w.TenantId == tenantId && w.ApartmentId == apartmentId);
+                .Where(w => w.TenantId == tenantId)
+                .AsNoTracking()
+                .Include(w => w.Apartment)
+                .ToListAsync();
+        }
+
+        public async Task<(bool IsSuccess, string Message)> RemoveApartmentFromWishListAsync(int tenantId, int apartmentId)
+        {
+            var target = await _context.WishListApartments.FirstOrDefaultAsync(w => w.TenantId == tenantId && w.ApartmentId == apartmentId);
+            if(target is null)
+                return (false, $"Apartment ID {apartmentId} was not found in Tenant ID {tenantId}'s wishlist.");
+            
+            _context.WishListApartments.Remove(target);
+            return (true, $"Apartment ID {apartmentId} has been removed from Tenant ID {tenantId}'s wishlist.");
         }
     }
 }
--- Sakani.Domain/Interfaces/IWishListRepository.cs
+++ Sakani.Domain/Interfaces/IWishListRepository.cs
@@ -7,7 +7,7 @@ namespace Sakani.Domain.Interfaces
     public interface IWishListRepository : IRepository<WishListApartment>
     {
         Task<IReadOnlyList<WishListApartment>> GetByTenantIdAsync(int tenantId);
-        Task<bool> ExistsAsync(int tenantId, int apartmentId);
-        Task<WishListApartment?> GetEntryAsync(int tenantId, int apartmentId);
+        Task<(bool IsSuccess, string Message)> ExistsAsync(int tenantId, int apartmentId);
+        Task<(bool IsSuccess, string Message)> RemoveApartmentFromWishListAsync(int tenantId, int apartmentId);
     }
 }
